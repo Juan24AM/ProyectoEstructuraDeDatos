b800af5a279281b4562e539e066ecb3934c706f5
SDFRPE <fernandez.shande@gmail.com>
2023-07-01 00:29:27 -0500

    Main Class

--- src/main/java/registrohotel/RegistroHotel.java
+++ src/main/java/registrohotel/RegistroHotel.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package me.ucv.registrohotel;
+package registrohotel;
 
-import me.ucv.registrohotel.interfaz.Acceder;
+import Interfaces.Acceder;
 
 /**
  *
