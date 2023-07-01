3c5f183125b1c0fb33857950ede760556c4c86f0
Juan <juanportocarreroriva@gmail.com>
2023-06-30 19:43:08 -0500

    Clase para consultar API datos.

---
+++ src/main/java/me/ucv/registrohotel/backend/ApiClient.java
@@ -0,0 +1,122 @@
+package me.ucv.registrohotel.backend;
+
+import java.io.IOException; // Libreria para controlar excepciones
+import java.net.HttpURLConnection; // Libreria para controlar realizar peticiones
+import java.net.URL; // Libreria para controlar darle formato a la url
+import java.util.Scanner;
+import org.json.JSONObject; // Libreria para manejar la respuesta de la API (json)
+
+public class ApiClient {
+    // Atributos
+    private String respuesta;
+    private String nombre;
+    private String paterno;
+    private String materno;
+    private String sexo;
+    private int nuEdad;
+
+    // Constructor
+    public ApiClient() {
+        respuesta = "9999";
+        nombre = null;
+        paterno = null;
+        materno = null;
+        sexo = null;
+        nuEdad = 0;
+    }
+
+    // Métodos
+    public String getRespuesta() {
+        return nombre;
+    }
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getNuEdad() {
+        return nuEdad;
+    }
+
+    public void setNuEdad(int nuEdad) {
+        this.nuEdad = nuEdad;
+    }
+    
+    // Metodo que establece los valores correspondientes a los atributos
+    public void buscarPorDNI(String dni) throws IOException {
+        String url = "https://hotelconsulta.azurewebsites.net/buscar/" + dni; // Contenamos el numero de DNI con la API
+
+        HttpURLConnection conn = null; // Se inicializa una variables para realizar la conexion en null
+        Scanner scanner = null; // Se inicializa variables para leer datos en null
+
+        try {
+            /* Se crea el objeto url, 
+            y se crea una conexión con el metodo openConnection,
+            el cual es asignado a la variable  conn */
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            // Se indica que la conexión sera de pito GET
+            conn.setRequestMethod("GET");
+            
+            scanner = new Scanner(conn.getInputStream());
+            StringBuilder response = new StringBuilder();
+            
+            while (scanner.hasNextLine()) {
+                response.append(scanner.nextLine());
+            }
+          
+            JSONObject jsonObject = new JSONObject(response.toString());
+            String coRespuesta = jsonObject.getString("coRespuesta");
+            String apePaterno = jsonObject.getString("apePaterno");
+            String apeMaterno = jsonObject.getString("apeMaterno");
+            String preNombres = jsonObject.getString("preNombres");
+            String Sexo = jsonObject.getString("sexo");
+            int edad = jsonObject.getInt("nuEdad");
+            setRespuesta(coRespuesta);
+            setNombre(apePaterno);
+            setPaterno(apeMaterno);
+            setMaterno(preNombres);
+            setSexo(Sexo);
+            setNuEdad(edad);
+            
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
