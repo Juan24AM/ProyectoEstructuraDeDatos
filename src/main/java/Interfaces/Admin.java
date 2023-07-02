c18dda304b6750ce74158be051b0ee299ed5b23b
SDFRPE <fernandez.shande@gmail.com>
2023-07-02 15:41:40 -0500

    Elimine imports innecesarios

--- src/main/java/Interfaces/Admin.java
+++ src/main/java/Interfaces/Admin.java
@@ -5,16 +5,8 @@
 package Interfaces;
 
 import backend.CallSQL;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
